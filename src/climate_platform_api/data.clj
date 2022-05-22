e3de12a60d86c71b3429b62b31a818bc2bf3552d
Malte Nottmeyer <nottmey@users.noreply.github.com>
2022-05-22 10:27:56 +0200

    prepare data parsing

--- src/climate_platform_api/data.clj
+++ src/climate_platform_api/data.clj
@@ -1,3 +1,13 @@
-(ns climate-platform-api.data)
+(ns climate-platform-api.data
+  (:require [clojure.java.io :as io]
+            [clojure.data.csv :as csv]))
 
 (def initial-data [])
+
+(comment
+  (with-open [reader (io/reader "resources/data/climate-change.csv")]
+    (let [csv-data (csv/read-csv reader)
+          csv-maps (map zipmap
+                        (repeat (first csv-data))
+                        (rest csv-data))]
+      (take 5 (doall 5 csv-maps)))))
