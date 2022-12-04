f1193900d0474c339a6ff7abbcaa2c8f1daa448b
Malte Nottmeyer <nottmey@users.noreply.github.com>
2022-12-04 22:23:47 +0100

    update region config

--- src/cdk/stack.clj
+++ src/cdk/stack.clj
@@ -1,17 +1,20 @@
 (ns cdk.stack
-  (:import (software.amazon.awscdk App RemovalPolicy Stack StackProps)
+  (:import (software.amazon.awscdk App Environment RemovalPolicy Stack StackProps)
            (software.amazon.awscdk.services.s3 Bucket$Builder)))
 
 (defn synth [opts]
   (let [app   (App.)
         props (-> (StackProps/builder)
+                  (.env (-> (Environment/builder)
+                            (.account (System/getenv "CDK_DEFAULT_ACCOUNT"))
+                            (.region (System/getenv "CDK_DEFAULT_REGION"))
+                            (.build)))
                   (.build))
         stack (Stack. app "HelloCdkStack" props)]
-    (->
-      (Bucket$Builder/create stack "MyFirstBucket")
-      (.versioned true)
-      (.removalPolicy RemovalPolicy/DESTROY)
-      (.autoDeleteObjects true)
-      (.build))
+    (-> (Bucket$Builder/create stack "MyFirstBucket")
+        (.versioned true)
+        (.removalPolicy RemovalPolicy/DESTROY)
+        (.autoDeleteObjects true)
+        (.build))
     (.synth app)))
 
