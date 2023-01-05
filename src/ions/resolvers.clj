83eb277e1b095d9e60184a5d2a6c4341311af5eb
Malte Nottmeyer <nottmey@users.noreply.github.com>
2023-01-05 20:54:23 +0100

    sort entity list by recently updated

--- src/ions/resolvers.clj
+++ src/ions/resolvers.clj
@@ -38,20 +38,37 @@
                     :arguments        {:database (first (utils/list-databases))
                                        :id       "0"}}))
 
-(defn db-entities [db schema]
-  (d/qseq {:query '[:find (pull ?e [*])
-                    :in $ [?as ...]
-                    :where [?e ?as]]
-           :args  [db (keys schema)]}))
+(defn recently-updated-entities [db schema]
+  (->> (d/q '[:find ?e (max ?t)
+              :in $ [?as ...]
+              :where [?e ?as _ ?t]]
+            db
+            (keys schema))
+       (sort-by second)
+       (reverse)
+       (map first)))
+
+(defn pull-entities [db es]
+  (->> es
+       (map-indexed vector)
+       (d/q '[:find ?i (pull ?e [*])
+              :in $ [[?i ?e]]] db)
+       (sort-by first)
+       (map second)))
+
+(comment
+  (let [db     (d/db (utils/get-connection (first (utils/list-databases))))
+        schema (utils/get-schema db)
+        es     (time (recently-updated-entities db schema))]
+    (count (time (doall (pull-entities db es))))))
 
 (defresolver datomic-resolve [:Query :list] [{:keys [arguments]}]
   (let [{:keys [database]} arguments
-        db          (d/db (utils/get-connection database))
-        schema      (utils/get-schema db)
-        db-entities (db-entities db schema)]
-    {"total" (count db-entities)
+        db     (d/db (utils/get-connection database))
+        schema (utils/get-schema db)]
+    {"total" (count (recently-updated-entities db schema))
      ; handing down context args to the field resolver (needed because parameterizable)
-     "page"  {"database" database}}))
+     "page"  {"database" database "t" (:t db)}}))
 
 (comment
   (datomic-resolve {:parent-type-name :Query
@@ -60,22 +77,23 @@
 
 (defresolver datomic-resolve [:EntityList :page] [{:keys [arguments parent-value]}]
   (let [{:keys [page size]} arguments
-        database    (get-in parent-value [:page :database])
-        db          (d/db (utils/get-connection database))
-        schema      (utils/get-schema db)
-        db-entities (db-entities db schema)
-        total       (count db-entities)
-        size        (min 100 (max 1 (or size 20)))
-        last        (if (pos? total)
-                      (dec (int (Math/ceil (/ total size))))
-                      0)
-        page        (min last (max 0 (or page 0)))
-        offset      (* page size)
-        entities    (->> db-entities
-                         (drop offset)
-                         (take size)
-                         (map first)
-                         (map #(mappings/map-entity % schema)))]
+        database (get-in parent-value [:page :database])
+        t        (get-in parent-value [:page :t])
+        db       (d/as-of (d/db (utils/get-connection database)) t)
+        schema   (utils/get-schema db)
+        entities (recently-updated-entities db schema)
+        total    (count entities)
+        size     (min 100 (max 1 (or size 20)))
+        last     (if (pos? total)
+                   (dec (int (Math/ceil (/ total size))))
+                   0)
+        page     (min last (max 0 (or page 0)))
+        offset   (* page size)
+        entities (->> entities
+                      (drop offset)
+                      (take size)
+                      (pull-entities db)
+                      (map #(mappings/map-entity % schema)))]
     {"info"     {"size"    size
                  "first"   0
                  "prev"    (if (pos? page) (dec page) nil)
@@ -88,7 +106,7 @@
   (let [database (first (utils/list-databases))]
     (time (datomic-resolve {:parent-type-name :EntityList
                             :field-name       :page
-                            :parent-value     {:total 60 :page {:database database}}
+                            :parent-value     {:total 60 :page {:database database :t 5}}
                             :arguments        {:page 0
                                                :size 20}}))))
 
