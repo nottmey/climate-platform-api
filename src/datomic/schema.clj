2bf9f3374ce14213ca1945b502859bc59542ec52
Malte Nottmeyer <nottmey@users.noreply.github.com>
2023-01-14 20:08:57 +0100

    add ensure data/schema util functions and docs

--- src/datomic/schema.clj
+++ src/datomic/schema.clj
@@ -1,53 +1,65 @@
 (ns datomic.schema
-  (:require [user :as u]
-            [datomic.client.api :as d]))
+  (:require [datomic.client.api :as d]
+            [user :as u]))
 
 ; TODO use transaction function to ensure type is complete
+
+(defn graphql-doc [text]
+  (str "Stored GraphQL Schema: " text))
+
 (def graphql-attributes
   [{:db/ident       :graphql.type/name
     :db/unique      :db.unique/identity
     :db/valueType   :db.type/string
-    :db/cardinality :db.cardinality/one}
+    :db/cardinality :db.cardinality/one
+    :db/doc         (graphql-doc "Name of this GraphQL type.")}
    {:db/ident       :graphql.type/deprecated?
     :db/valueType   :db.type/boolean
-    :db/cardinality :db.cardinality/one}
-   ;; relation
-   ; deprecated?
+    :db/cardinality :db.cardinality/one
+    :db/doc         (graphql-doc "Whether this GraphQL type is deprecated or not. Default: `false`.")}
    {:db/ident       :graphql.relation/deprecated?
     :db/valueType   :db.type/boolean
-    :db/cardinality :db.cardinality/one}
-   ; source-type
+    :db/cardinality :db.cardinality/one
+    :db/doc         (graphql-doc "Whether the GraphQL field of this GraphQL-field-to-Datomic-attribute relation is deprecated or not. Default: `false`.")}
    {:db/ident       :graphql.relation/type
     :db/valueType   :db.type/ref
-    :db/cardinality :db.cardinality/one}
-   ; forward-field
+    :db/cardinality :db.cardinality/one
+    :db/doc         (graphql-doc "GraphQL type in which the GraphQL field of this GraphQL-field-to-Datomic-attribute relation is contained.")}
    {:db/ident       :graphql.relation/field
     :db/valueType   :db.type/string
-    :db/cardinality :db.cardinality/one}
-   ; type+field
+    :db/cardinality :db.cardinality/one
+    :db/doc         (graphql-doc "Field name of this GraphQL-field-to-Datomic-attribute relation.")}
    {:db/ident       :graphql.relation/type+field
     :db/unique      :db.unique/identity
     :db/valueType   :db.type/tuple
     :db/tupleAttrs  [:graphql.relation/type
                      :graphql.relation/field]
-    :db/cardinality :db.cardinality/one}
-   ; destination-type, optional
+    :db/cardinality :db.cardinality/one
+    :db/doc         (graphql-doc "Unique automatically managed GraphQL-type-and-field-name tuple of this GraphQL-field-to-Datomic-attribute relation.")}
    {:db/ident       :graphql.relation/target
     :db/valueType   :db.type/ref
-    :db/cardinality :db.cardinality/one}
-   ; attribute
+    :db/cardinality :db.cardinality/one
+    :db/doc         (graphql-doc "GraphQL type which is targeted by this GraphQL-field-to-Datomic-attribute relation, iff it is a reference to another entity.")}
    {:db/ident       :graphql.relation/attribute
     :db/valueType   :db.type/ref
-    :db/cardinality :db.cardinality/one}
-   ; forward?
+    :db/cardinality :db.cardinality/one
+    :db/doc         (graphql-doc "Datomic attribute which is pulled, when this relations type and field is requested in the GraphQL API.")}
    {:db/ident       :graphql.relation/forward?
     :db/valueType   :db.type/boolean
-    :db/cardinality :db.cardinality/one}])
+    :db/cardinality :db.cardinality/one
+    :db/doc         (graphql-doc "Whether the forward or backwards reference of this relations reference attribute should be pulled, when requested in the GraphQL API. Default: `true`.")}])
 
 (comment
-  (d/transact
-    (u/sandbox-conn)
-    {:tx-data graphql-attributes}))
+  (u/ensure-schema graphql-attributes))
+
+(def platform-attributes
+  [{:db/ident       :platform/name
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Name of any non-user platform entity."}])
+
+(comment
+  (u/ensure-schema platform-attributes))
 
 (defn add-value-field-tx-data [type-name field-name attribute]
   [{:db/id             type-name
@@ -57,10 +69,8 @@
     :graphql.relation/field     field-name}])
 
 (comment
-  (d/transact
-    (u/sandbox-conn)
-    {:tx-data (add-value-field-tx-data "SomeTypeY" "identX" :db/ident)}))
-
+  (u/ensure-data
+    (add-value-field-tx-data "SomeTypeY" "ident" :db/ident)))
 
 (defn add-ref-field-tx-data [type-name field-name target-type attribute forward?]
   [{:db/id             type-name
--- src/user.clj
+++ src/user.clj
@@ -1,60 +1,118 @@
 (ns user
-  (:require [datomic.access :as access]
-            [datomic.client.api :as d]))
+  (:require [clojure.set :as set]
+            [datomic.access :as access]
+            [datomic.client.api :as d])
+  (:import (clojure.lang ExceptionInfo)))
 
-(def sandbox "sandbox")
+(def sandbox-env-db-name "sandbox")
 
 (defn sandbox-conn []
-  (access/get-connection sandbox))
+  (access/get-connection sandbox-env-db-name))
 
 (defn sandbox-db []
   (d/db (sandbox-conn)))
 
 (defn create-sandbox []
-  (d/create-database (access/get-client) {:db-name sandbox}))
+  (d/create-database (access/get-client) {:db-name sandbox-env-db-name}))
 
 (comment
   (create-sandbox))
 
 ; TODO backup data regularly and test recovery
 (defn delete-sandbox []
-  (d/delete-database (access/get-client) {:db-name sandbox}))
+  (d/delete-database (access/get-client) {:db-name sandbox-env-db-name}))
 
 (comment
   (delete-sandbox))
 
-(defn get-sandbox-tx-log [n]
-  (let [conn     (access/get-connection sandbox)
-        db       (d/db conn)
-        start    (->> (d/qseq '[:find ?tx :where [?tx :db/txInstant]] db)
-                      (map first)
-                      sort
-                      reverse
-                      (drop (dec n))
-                      (first))
-        idents   (->> (d/q '[:find ?e ?ident
-                             :where [?e :db/ident ?ident]] db)
-                      (into {}))
-        identify #(get idents % %)]
-    (->> (d/tx-range conn {:start start})
-         (map :data)
-         (map #(map
-                 (fn [[e a v add? tx]]
-                   [(identify e) (identify a) (identify v) add? tx])
-                 %)))))
+(defn empty-tx-result [conn]
+  (let [db (d/db conn)]
+    {:db-before db
+     :db-after  db
+     :tx-data   []
+     :tempids   {}}))
+
+(defn ensure-schema
+  ([tx-data] (ensure-schema tx-data sandbox-env-db-name))
+  ([tx-data db-name]
+   {:pre [(every? map? tx-data)
+          (every? :db/ident tx-data)]}
+   (let [conn           (access/get-connection db-name)
+         test-tx-result (d/with (d/with-db conn) {:tx-data tx-data})]
+     (if (> 1 (count (:tx-data test-tx-result)))
+       (empty-tx-result conn)
+       (d/transact conn {:tx-data tx-data})))))
+
+(defn ensure-data
+  ([tx-data] (ensure-data tx-data sandbox-env-db-name))
+  ([tx-data db-name]
+   {:pre [(every? map? tx-data)]}
+   (let [conn               (access/get-connection db-name)
+         schema             (access/get-schema (d/db conn))
+         unique-attrs       (->> (vals schema)
+                                 (filter :db/unique)
+                                 (map :db/ident)
+                                 (map hash-set))
+         unique-tuple-attrs (->> (vals schema)
+                                 (filter :db/unique)
+                                 (filter :db/tupleAttrs)
+                                 (map :db/tupleAttrs)
+                                 (map set))
+         uniques-sets       (concat unique-attrs unique-tuple-attrs)
+         has-uniqueness?    (fn [tx-map]
+                              (let [tx-keys (into #{} (keys tx-map))]
+                                (some #(set/superset? tx-keys %) uniques-sets)))]
+     (assert (every? has-uniqueness? tx-data))
+     (try
+       (let [test-tx-result (d/with (d/with-db conn) {:tx-data tx-data})]
+         (if (> 1 (count (:tx-data test-tx-result)))
+           (empty-tx-result conn)
+           (d/transact conn {:tx-data tx-data})))
+       (catch ExceptionInfo e
+         (if (= (:db/error (ex-data e))
+                :db.error/unique-conflict)
+           (empty-tx-result conn)
+           (throw e)))))))
+
+(defn get-tx-log
+  ([] (get-tx-log 1))
+  ([n] (get-tx-log n sandbox-env-db-name))
+  ([n db-name]
+   (let [conn     (access/get-connection db-name)
+         db       (d/db conn)
+         start    (->> (d/qseq '[:find ?tx :where [?tx :db/txInstant]] db)
+                       (map first)
+                       sort
+                       reverse
+                       (drop (dec n))
+                       (first))
+         idents   (->> (d/q '[:find ?e ?ident
+                              :where [?e :db/ident ?ident]] db)
+                       (into {}))
+         identify #(get idents % %)]
+     (->> (d/tx-range conn {:start start})
+          (map :data)
+          (map #(map
+                  (fn [[e a v add? tx]]
+                    [(identify e) (identify a) (identify v) add? tx])
+                  %))))))
 
 (comment
-  (get-sandbox-tx-log 1))
+  (get-tx-log 1 #_access/dev-env-db-name))
 
-(defn get-sandbox-stats []
-  (let [db (d/db (access/get-connection sandbox))]
-    (d/db-stats db)))
-
-(comment
-  (get-sandbox-stats))
-
-(defn get-sandbox-schema []
-  (access/get-schema (d/db (access/get-connection sandbox))))
+(defn get-db-stats
+  ([] (get-db-stats sandbox-env-db-name))
+  ([db-name]
+   (let [db (d/db (access/get-connection db-name))]
+     (d/db-stats db))))
 
 (comment
-  (get-sandbox-schema))
+  (get-db-stats #_access/dev-env-db-name))
+
+(defn get-schema
+  ([] (get-schema sandbox-env-db-name))
+  ([db-name]
+   (access/get-schema (d/db (access/get-connection db-name)))))
+
+(comment
+  (get-schema #_access/dev-env-db-name))
