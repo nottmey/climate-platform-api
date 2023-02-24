cad4b26c3e6e668d5b85db68aed5d9b1d986023d
Malte Nottmeyer <nottmey@users.noreply.github.com>
2023-02-24 18:40:45 +0100

    docs: add todo for refactoring `resolves-graphql-field?`

--- src/shared/operations/operation.clj
+++ src/shared/operations/operation.clj
@@ -7,5 +7,6 @@
   (get-graphql-parent-type [this] "Returns GraphQL parent type.")
   (gen-graphql-field [this entity] "Generates GraphQL field data for a given entity type.")
   (gen-graphql-object-types [this entity] "Generates all operation individual object types. Returns `nil`, if there are none.")
+  ; TODO resolves-graphql-field? can be replaced by generated index (since we fetch the schema anyway)
   (resolves-graphql-field? [this field-name] "Returns whether a specific (entity) field belongs to this operation.")
   (resolve-field-data [this conn resolver-args] "Resolves entity field data for specific field matched by `resolves-graphql-field?`."))
