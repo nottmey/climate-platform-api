48a0fb354a8fe8efe2bbc81cdbde4cbdb4f425ab
Malte Nottmeyer <nottmey@users.noreply.github.com>
2023-02-24 18:42:48 +0100

    docs: add todo for refactoring schema use

--- src/shared/operations/operation.clj
+++ src/shared/operations/operation.clj
@@ -9,4 +9,5 @@
   (gen-graphql-object-types [this entity] "Generates all operation individual object types. Returns `nil`, if there are none.")
   ; TODO resolves-graphql-field? can be replaced by generated index (since we fetch the schema anyway)
   (resolves-graphql-field? [this field-name] "Returns whether a specific (entity) field belongs to this operation.")
+  ; TODO fetch schema upfront resolve-field-data (allows caching) and pass it into it (since we fetch it anyway)
   (resolve-field-data [this conn resolver-args] "Resolves entity field data for specific field matched by `resolves-graphql-field?`."))
